b71410a9e3fec94f6212f1e18f69d7ee9f9f9831
JLUAS <jlurquieta1@gmail.com>
2025-01-31 23:51:51 -0600

    mime

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mime';
+  nombre: string = '';  // Variable para almacenar el input
+  esNombreValido: boolean | null = null; // Variable para mostrar el resultado
+  numPag:number = 1
+  surpristeToggle:boolean=false
+  validarNombre() {
+    this.esNombreValido = this.nombre.trim().toLowerCase() === 'raabta';
+  }
+  add(){
+    this.numPag = this.numPag + 1
+  }
+  sub(){
+    if(this.numPag=1){
+      this.numPag=1
+    }else{
+      this.numPag = this.numPag -1
+    }
+  }
+  surprise(){
+    this.surpristeToggle=true
+  }
 }
